77ae965cddf4f6cec3d907bf6b9e53e3ba0beab6
Sarthak Raghuvanshi <sarthak.raghuvanshiu7702@gmail.com>
2023-07-31 17:31:26 +0530

    added message in sum.java

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class  Sum{
+
+    Public static void public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+system.out.println("The Sum is" + (a+b));;
+    }
+    
+}
